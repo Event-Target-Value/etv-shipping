0cf6529edb27d0309f435e3bf854ca8dd0ef4f72
Akshaj Mody <akshajmody@gmail.com>
2020-05-01 20:12:08 -0700

    AWS hosting + randomized 100 data entries

--- server/db/seeder.js
+++ server/db/seeder.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 const save = require(path.join(__dirname, 'productsdb.js')).save;
 const findAllProducts = require(path.join(__dirname, 'productsdb.js')).findAllProducts;
-const dummy = require(path.join(__dirname, 'dummydata.js'));
+const seedFile = require(path.join(__dirname, 'dummydata.js'));
 
 
-save(dummy.dummyData, (err, data) => {
+save(seedFile.seedData, (err, data) => {
   if (err) {
-    console.log('SAVING ERROR' + err);
+    console.log('SAVING ERROR: ' + err);
   } else {
-    console.log('SEEDED TO DATABASE');
+    console.log('SEEDED 100 RANDOMIZED ENTIRES TO DATABASE');
   }
 });
